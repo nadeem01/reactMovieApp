73d9b78cddb765834327a3922230bc10b6a466c4
Jamil Abbas <jamil.abbas813@gmail.com>
2019-03-10 19:02:07 +0500

    First commit

---
+++ src/components/elements/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>That Page doesn't exists!!!</h1>
+        </div>
+    )
+}
+
+export default NotFound;  
